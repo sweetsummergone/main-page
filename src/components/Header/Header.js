52b25336c3491dec37fcb139d5c0111f2ccd4dcd
sweetsummergone <limfix.13@gmail.com>
2022-08-20 20:03:57 +0300

    fix: route header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 export default function Header() {
     return (
         <header class="header">
-            <Link className="header__link" to="/">Home</Link>
+            <Link className="header__link" to="main-page">Home</Link>
             <Link className="header__link" to="projects">Projects</Link>
         </header>
     )
